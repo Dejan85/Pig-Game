fab6f2cfb60d94b6ab47eda88fbbd67cbb43fe56
xypnotica <xypnotica@gmail.com>
2019-11-26 21:13:40 +0100

    update work

--- src/somponents/App.js
+++ src/somponents/App.js
@@ -1,16 +1,5 @@
-// var cube = document.getElementById('cube');
+import useDomHook from './dom/useDomHook';
 
-// var min = 1;
-// var max = 24;
+const { dom } = useDomHook();
 
-// cube.onclick = function() {
-// 	var xRand = getRandom(max, min);
-// 	var yRand = getRandom(max, min);
-
-// 	cube.style.webkitTransform = 'rotateX(' + xRand + 'deg) rotateY(' + yRand + 'deg)';
-// 	cube.style.transform = 'rotateX(' + xRand + 'deg) rotateY(' + yRand + 'deg)';
-// };
-
-// function getRandom(max, min) {
-// 	return (Math.floor(Math.random() * (max - min)) + min) * 90;
-// }
+// dom.gamePlayDom();
